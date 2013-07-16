a1f476429fb226a3e1747eef2dce5bc7d3821a7c
Aaron Baldwin <baldwina@wwidea.org>
2013-07-16 16:30:04 -0600

    add parents controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 AllowMe::Application.routes.draw do
+  
+  resources :parents, only: %i(new create)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
